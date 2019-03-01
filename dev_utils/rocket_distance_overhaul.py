c86083c5cb2f335469bcd41fd6784c377094ad4d
Chalant <strikerklm96@gmail.com>
2019-02-28 20:08:23 -0600

    overhaul

--- dev_utils/rocket_distance_overhaul.py
+++ dev_utils/rocket_distance_overhaul.py
@@ -5,28 +5,59 @@
 import math
 import matplotlib.pyplot as plt
 import pylab
+import copy
 from collections import deque
 
-# what is on your rocket?
-cargo_bays = 2
-other_modules = 0
 
-# must be in descending efficiency! km of range per kg of fuel burned
-fuel_efficiencies = [110, 80, 60, 40]
+class Rocket:
+	def __init__(self, fuel_efficiency, oxidizer_efficiency, engine_mass, cargo_bays=0, engine_penalty=0, other_modules=0):
+		self.fuel_efficiency = fuel_efficiency
+		self.oxidizer_efficiency = oxidizer_efficiency
+		self.engine_mass = engine_mass
+		self.engine_penalty = engine_penalty
+		self.cargo_bays = cargo_bays
+		self.other_modules = other_modules
+		self.max_fuel = 999999
 
-# oxylite is 1.0, lox is 1.33
-oxidizer_efficiency = 1.33
 
-stop_graph_at_peak_range: bool = True
+def make_rockets(num: int) -> [Rocket]:
+	rocket_copies: [Rocket] = []
+	for i in range(num):
+		rocket_copies.append(Rocket(60, 1.33, engine_mass=2000, cargo_bays=0, engine_penalty=0, other_modules=5))
+	return rocket_copies
+
+
+# your furthest rocket should go first!
+rockets: [Rocket] = make_rockets(4)
+
+rockets[0].fuel_efficiency = 3
+rockets[0].cargo_bays = 0
+rockets[0].engine_penalty = 0
+
+rockets[1].fuel_efficiency = 2.25
+rockets[1].cargo_bays = 0
+rockets[1].engine_penalty = 0
+
+rockets[2].fuel_efficiency = 2
+rockets[2].cargo_bays = 0
+rockets[2].engine_penalty = 0
+
+rockets[3].fuel_efficiency = 2
+rockets[3].cargo_bays = 0
+rockets[3].engine_penalty = 0
+rockets[3].max_fuel = 900
+rockets[3].engine_mass = 0
+
+stop_graph_at_peak_range: bool = False
 fuel_tank_capacity = 900
 oxy_tank_capacity = 2700
 empty_tank_mass = 100
 
 capsule_mass = 200
-engine_mass = 500
-
-cargo_bay_mass = 2000
+cargo_bay_mass = 1000
 other_module_mass = 200
+exponent = 1.5
+weight_divisor = 40.0
 
 
 def get_num_tanks(amount: float, amount_per: float) -> int:
@@ -41,35 +72,35 @@ def get_num_oxy_tanks(kg_oxy: float) -> int:
 	return get_num_tanks(kg_oxy, oxy_tank_capacity)
 
 
-def get_weight(kg_fuel: float, kg_oxy: float, cargo_bays_units: int, other_modules_units: int) -> float:
+def get_weight(kg_fuel: float, kg_oxy: float, rocket: Rocket) -> float:
 	num_fuel_tanks = get_num_fuel_tanks(kg_fuel)
 	num_oxy_tanks = get_num_oxy_tanks(kg_oxy)
 
 	fuel = num_fuel_tanks * empty_tank_mass + kg_fuel
 	oxidizer = num_oxy_tanks * empty_tank_mass + kg_oxy
-	capsule_engine = capsule_mass + engine_mass
-	storage = cargo_bays_units * cargo_bay_mass
-	other = other_modules_units * other_module_mass
+	capsule_engine = capsule_mass + rocket.engine_mass
+	storage = rocket.cargo_bays * cargo_bay_mass
+	other = rocket.other_modules * other_module_mass
 	return fuel + oxidizer + capsule_engine + storage + other
 
 
 def distance_penalty(weight: float) -> float:
 	weight: float = float(weight)
-	return max(weight, (weight / 300.0) ** 3.2)
+	return max(weight, (weight / float(weight_divisor)) ** float(exponent))
 
 
-def distance(kg_fuel: float, kg_oxy: float, km_per_kg_fuel: float) -> float:
-	return oxidizer_efficiency * min(kg_fuel, kg_oxy) * km_per_kg_fuel
+def distance(kg_fuel: float, kg_oxy: float, km_per_kg_fuel: float, oxy_efficiency: float) -> float:
+	return float((min(kg_fuel, kg_oxy))) * float(km_per_kg_fuel) * float(oxy_efficiency)
 
 
-def calc_total_distance(kg_fuel: float, km_per_kg_fuel: float):
+def calc_total_distance(kg_fuel: float, rocket: Rocket):
 	kg_oxy = kg_fuel
 
-	weight = get_weight(kg_fuel, kg_oxy, cargo_bays, other_modules)
+	weight = get_weight(kg_fuel, kg_oxy, rocket)
 	penalty = distance_penalty(weight)
 
-	max_dist = distance(kg_fuel, kg_oxy, km_per_kg_fuel)
-	final_distance = max_dist - penalty
+	max_dist = distance(kg_fuel, kg_oxy, rocket.fuel_efficiency, rocket.oxidizer_efficiency)
+	final_distance = max_dist - penalty - rocket.engine_penalty
 	return final_distance
 
 
@@ -86,13 +117,16 @@ def only_gets_worse(distance_delta_queue: deque) -> bool:
 	a = distance_delta_queue[0]
 	b = distance_delta_queue[1]
 	c = distance_delta_queue[2]
-	d = distance_delta_queue[3]
 
 	is_getting_worse = 0 > a
 	if stop_graph_at_peak_range:
 		is_getting_worse: bool = True
 
-	return is_getting_worse and a > b > c > d
+	return is_getting_worse and a > b > c
+
+
+def build_legend(rocket: Rocket) -> str:
+	return '[' + str(rocket.fuel_efficiency) + ',' + str(rocket.engine_mass) + ',' + str(rocket.cargo_bays) + ']'
 
 
 def main():
@@ -100,16 +134,18 @@ def main():
 	max_distance = 0
 	distance_delta_queue: deque = deque()
 
-	for efficiency in fuel_efficiencies:
-		is_most_efficient_fuel: bool = efficiency == fuel_efficiencies[0]
+	for rocket in rockets:
 		x_axis: [int] = []
 		y_axis: [int] = []
 		for fuel_amount in range(max_fuel_amount):
-			new_range = calc_total_distance(fuel_amount, efficiency)
+			if fuel_amount > rocket.max_fuel:
+				break
+
+			new_range = calc_total_distance(fuel_amount, rocket)
 
 			distance_delta_queue.append(new_range)
-			if len(distance_delta_queue) >= 4:
-				if is_most_efficient_fuel and only_gets_worse(distance_delta_queue):
+			if len(distance_delta_queue) >= 3:
+				if max_distance == 0 and only_gets_worse(distance_delta_queue):
 					max_distance = new_range
 					max_fuel_amount = fuel_amount
 					break  # rocket distance will never go up again
@@ -122,17 +158,12 @@ def main():
 		if len(x_axis) == 0:
 			print('Your rocket was so bad that it cant even get 1 km of range! Try improving fuel efficiency or reducing weight.')
 
-		plt.plot(x_axis, y_axis, label=str(efficiency) + 'km per kg')
+		plt.plot(x_axis, y_axis, label=build_legend(rocket))
 
 	plt.xlabel('Fuel in Kg')
 	plt.ylabel('Range in Km')
 
-	pylab.legend(title='Fuel Efficiency', loc='upper left')
-
-	plt.text(max_fuel_amount * 0.25, max_distance * 1.12, 'Cargo Bays: ' + str(cargo_bays), ha='center', va='center')
-	plt.text(max_fuel_amount * 0.25, max_distance * 1.18, 'Other Modules: ' + str(other_modules), ha='center', va='center')
-	plt.text(max_fuel_amount * 0.75, max_distance * 1.12, 'Engine Mass: ' + str(engine_mass) + 'kg', ha='center', va='center')
-	plt.text(max_fuel_amount * 0.75, max_distance * 1.18, 'Oxidizer Boost: ' + str(oxidizer_efficiency) + 'x', ha='center', va='center')
+	pylab.legend(title='[Efficiency,EngineMass,CargoBays]', loc='upper left')
 
 	plt.show()
 
