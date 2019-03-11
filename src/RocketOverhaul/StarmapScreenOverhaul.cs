f968f1b1818847b4bb37a008ad7060f95b287fdf
Chalant <strikerklm96@gmail.com>
2019-03-10 23:51:30 -0500

    star screen improved!

--- src/RocketOverhaul/StarmapScreenOverhaul.cs
+++ src/RocketOverhaul/StarmapScreenOverhaul.cs
@@ -9,6 +9,13 @@ namespace RocketOverhaul
 	{
 		public class Caption
 		{
+			public static string TotalOxidizableFuel = "Total Oxidizable Fuel";
+			public static string EngineExhaustVelocity = "Engine Exhaust Velocity";
+			public static string AverageOxidizerEfficiency = "Average Oxidizer Efficiency";
+
+			public static string TotalEngineThrust = "Main Engine";
+			public static string TotalBoosterThrust = "Solid Boosters";
+			public static string Payload = "Payload";
 			public static string TotalDistance = "Total Range";
 		}
 
@@ -17,8 +24,15 @@ namespace RocketOverhaul
 		/// </summary>
 		public static string FormatDistance(float distanceInKm)
 		{
-			return distanceInKm + Unit;
+			string sign = "+";
+			if (distanceInKm < 0)
+			{
+				sign = "";
+			}
+
+			return sign + string.Format(FormatNoDecimalAndCommas, distanceInKm) + Unit;
 		}
-		private static string Unit = "km";
+		private static readonly string Unit = "km";
+		public static readonly string FormatNoDecimalAndCommas = "{0:n0}"; // no decimals and use , seperators for thousands place
 	}
 }
--- src/RocketOverhaul/StarmapScreenPatches.cs
+++ src/RocketOverhaul/StarmapScreenPatches.cs
@@ -26,37 +26,79 @@ namespace RocketOverhaul
 				__instance.Get(out BreakdownList rocketDetailsRange, nameof(rocketDetailsRange));
 				__instance.Get(out RocketThrustWidget rocketThrustWidget, nameof(rocketThrustWidget));
 				__instance.Get(out CommandModule currentCommandModule, nameof(currentCommandModule));
+				RocketStatsOverhaul stats = (RocketStatsOverhaul)currentCommandModule.rocketStats;
+				RocketEngineImproved engine = (RocketEngineImproved)stats.GetMainEngine();
+
+				string engineEfficiency = string.Format(StarmapScreenOverhaul.FormatNoDecimalAndCommas, engine.ExhaustVelocity) + "m/s";
+				string engineThrust = StarmapScreenOverhaul.FormatDistance(stats.GetEngineThrust());
+				string boosterThrust = StarmapScreenOverhaul.FormatDistance(stats.GetBoosterThrust());
+				string modulePenalty = StarmapScreenOverhaul.FormatDistance(-stats.GetModulePenalty());
+				string totalDistance = StarmapScreenOverhaul.FormatDistance(stats.GetRocketMaxDistance());
+				Color orange = new Color(1f, 0.55f, 0f);
 
 				rocketDetailsRange.ClearRows();
 
-				rocketDetailsRange.AddRow().ShowData(STRINGS.UI.STARMAP.ROCKETSTATS.TOTAL_OXIDIZABLE_FUEL, GameUtil.GetFormattedMass(currentCommandModule.rocketStats.GetTotalOxidizableFuel(), GameUtil.TimeSlice.None, GameUtil.MetricMassFormat.UseThreshold, true, "{0:0.#}"));
-				rocketDetailsRange.AddRow().ShowData(STRINGS.UI.STARMAP.ROCKETSTATS.ENGINE_EFFICIENCY, GameUtil.GetFormattedEngineEfficiency(currentCommandModule.rocketStats.GetEngineEfficiency()));
-				rocketDetailsRange.AddRow().ShowData(STRINGS.UI.STARMAP.ROCKETSTATS.OXIDIZER_EFFICIENCY, GameUtil.GetFormattedPercent(currentCommandModule.rocketStats.GetAverageOxidizerEfficiency(), GameUtil.TimeSlice.None));
+				rocketDetailsRange.AddRow().ShowData(StarmapScreenOverhaul.Caption.TotalOxidizableFuel, GameUtil.GetFormattedMass(stats.GetTotalOxidizableFuel(), GameUtil.TimeSlice.None, GameUtil.MetricMassFormat.UseThreshold, true, "{0:0.#}"));
+				rocketDetailsRange.AddRow().ShowData(StarmapScreenOverhaul.Caption.EngineExhaustVelocity, engineEfficiency);
+				rocketDetailsRange.AddRow().ShowData(StarmapScreenOverhaul.Caption.AverageOxidizerEfficiency, GameUtil.GetFormattedPercent(stats.GetAverageOxidizerEfficiency(), GameUtil.TimeSlice.None));
 
-				float meters = currentCommandModule.rocketStats.GetBoosterThrust() * 1000f;
-				if (meters != 0.0)
-					rocketDetailsRange.AddRow().ShowData(STRINGS.UI.STARMAP.ROCKETSTATS.SOLID_BOOSTER, GameUtil.GetFormattedDistance(meters));
+				BreakdownListRow row;
+				row = rocketDetailsRange.AddRow();
+				row.ShowStatusData(StarmapScreenOverhaul.Caption.TotalEngineThrust, engineThrust, Color.green);
+				row.SetImportant(true);
 
-				BreakdownListRow breakdownListRow1 = rocketDetailsRange.AddRow();
-				breakdownListRow1.ShowStatusData(STRINGS.UI.STARMAP.ROCKETSTATS.TOTAL_THRUST, GameUtil.GetFormattedDistance(currentCommandModule.rocketStats.GetTotalThrust() * 1000f), Color.green);
-				breakdownListRow1.SetImportant(true);
+				row = rocketDetailsRange.AddRow();
+				row.ShowStatusData(StarmapScreenOverhaul.Caption.TotalBoosterThrust, boosterThrust, Color.green);
+				row.SetImportant(true);
 
-				rocketThrustWidget.gameObject.SetActive(true);
+				row = rocketDetailsRange.AddRow();
+				row.ShowStatusData(StarmapScreenOverhaul.Caption.Payload, modulePenalty, Color.red);
+				row.SetImportant(true);
 
-				BreakdownListRow breakdownListRow2 = rocketDetailsRange.AddRow();
-				breakdownListRow2.ShowStatusData((string)STRINGS.UI.STARMAP.ROCKETSTATUS.WEIGHTPENALTY, "hi", Color.red);
-				breakdownListRow2.SetHighlighted(true);
-
-				// graph
-				rocketDetailsRange.AddCustomRow(rocketThrustWidget.gameObject);
-				rocketThrustWidget.Draw(currentCommandModule);
-
-				BreakdownListRow breakdownListRow3 = rocketDetailsRange.AddRow();
-				breakdownListRow3.ShowData(StarmapScreenOverhaul.Caption.TotalDistance, StarmapScreenOverhaul.FormatDistance(currentCommandModule.rocketStats.GetRocketMaxDistance()));
-				breakdownListRow3.SetImportant(true);
+				row = rocketDetailsRange.AddRow();
+				row.ShowStatusData(StarmapScreenOverhaul.Caption.TotalDistance, totalDistance, orange);
+				row.SetImportant(true);
 			}
 		}
 
+		[HarmonyPatch(typeof(StarmapScreen))]
+		[HarmonyPatch("UpdateStorageDisplay")]
+		public static class UpdateStorageDisplay
+		{
+			static bool Prefix() { return true; } // skip original method
+			static void Postfix(StarmapScreen __instance)
+			{
+				__instance.Get(out BreakdownList rocketDetailsStorage, nameof(rocketDetailsStorage));
+				__instance.Get(out CommandModule currentCommandModule, nameof(currentCommandModule));
+				RocketStatsOverhaul stats = (RocketStatsOverhaul)currentCommandModule.rocketStats;
+
+				rocketDetailsStorage.SetTitle(StarmapScreenOverhaul.Caption.Payload);
+				rocketDetailsStorage.ClearRows();
+
+				// add row for each module
+				foreach (GameObject gameObject in stats.BuildingNetworkEnumerable())
+				{
+					string penalty = null;
+					if (gameObject.GetComponent<CargoBay>() != null)
+						penalty = StarmapScreenOverhaul.FormatDistance(-ModulePenalties.CargoPenalty);
+					else if (gameObject.GetComponent<TouristModule>() != null)
+						penalty = StarmapScreenOverhaul.FormatDistance(-ModulePenalties.TouristPenalty);
+					else if (gameObject.GetComponent<ResearchModule>() != null)
+						penalty = StarmapScreenOverhaul.FormatDistance(-ModulePenalties.ResearchPenalty);
+
+					if (penalty != null) // was this a module that we are counting?
+					{
+						string moduleName = gameObject.gameObject.GetProperName();
+						rocketDetailsStorage.AddRow().ShowData(moduleName, penalty);
+					}
+				}
+			}
+		}
+
+		/// <summary>
+		/// Easily get a private instance field of type T from StarmapScreen.
+		/// Note that something like an int can't be set via this.
+		/// </summary>
 		public static void Get<T>(this StarmapScreen screen, out T value, string fieldName)
 		{
 			Type type = screen.GetType();
