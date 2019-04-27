30aebe67c1d5639323914b62a9b5fdce34084cc8
Chalant <strikerklm96@gmail.com>
2019-04-27 15:14:35 -0500

    miner works!

--- src/AdvancedRoboMiner/AutoMinerPatches.cs
+++ src/AdvancedRoboMiner/AutoMinerPatches.cs
@@ -18,5 +18,16 @@ namespace AdvancedRoboMiner
 				__result = Grid.Solid[cell] && !Grid.Foundation[cell] && Grid.Element[cell].hardness < AdvancedRoboMiner.MaxHardness;
 			}
 		}
+
+		[HarmonyPatch(typeof(AutoMiner))]
+		[HarmonyPatch(nameof(AutoMiner.DigBlockingCB))]
+		public static class DigBlockingCB
+		{
+			static bool Prefix() { return true; } // skip original method
+			static void Postfix(int cell, ref bool __result)
+			{
+				__result = Grid.Foundation[cell] || Grid.Element[cell].hardness >= AdvancedRoboMiner.MaxHardness;
+			}
+		}
 	}
 }
