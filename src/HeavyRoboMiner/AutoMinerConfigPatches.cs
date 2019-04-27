2ad1955a14515b7e3789a7a5d073b79e3b003744
Chalant <strikerklm96@gmail.com>
2019-04-27 17:09:19 -0500

    start heavy robo miner

---
+++ src/HeavyRoboMiner/AutoMinerConfigPatches.cs
@@ -0,0 +1,67 @@
+﻿/// <summary>
+/// Created by lfricken https://github.com/lfricken/oni-mods
+/// </summary>
+
+using Harmony;
+using TUNING;
+using UnityEngine;
+
+namespace AdvancedRoboMiner
+{
+	class AutoMinerConfigPatches
+	{
+		//[HarmonyPatch(typeof(AutoMinerConfig))]
+		//[HarmonyPatch(nameof(AutoMinerConfig.CreateBuildingDef))]
+		//public static class CreateBuildingDef
+		//{
+		//	static bool Prefix() { return false; } // skip original method
+		//	static void Postfix(ref BuildingDef __result)
+		//	{
+		//		__result.EnergyConsumptionWhenActive = AdvancedRoboMiner.EnergyConsumption;
+		//		__result.SelfHeatKilowattsWhenActive = AdvancedRoboMiner.HeatProduction;
+		//	}
+		//}
+
+		[HarmonyPatch(typeof(AutoMinerConfig))]
+		[HarmonyPatch(nameof(AutoMinerConfig.DoPostConfigureComplete))]
+		public static class DoPostConfigureComplete
+		{
+			static bool Prefix() { return true; }
+			static void Postfix(GameObject go)
+			{
+				GeneratedBuildings.RegisterLogicPorts(go, LogicOperationalController.INPUT_PORTS_0_0);
+				go.AddOrGet<LogicOperationalController>();
+				AutoMiner autoMiner = go.AddOrGet<AutoMiner>();
+				autoMiner.width = AdvancedRoboMiner.Range.width;
+				autoMiner.height = AdvancedRoboMiner.Range.height;
+				autoMiner.x = AdvancedRoboMiner.Range.xOffset;
+				autoMiner.y = 0;
+				autoMiner.vision_offset = AdvancedRoboMiner.Range.VisionOffset;
+
+				AddVisualizer(go, false);
+			}
+		}
+
+		[HarmonyPatch(typeof(AutoMinerConfig))]
+		[HarmonyPatch(nameof(AddVisualizer))]
+		public static class AddVisualizerPatch
+		{
+			static bool Prefix() { return true; }
+			static void Postfix(GameObject prefab, bool movable)
+			{
+				AddVisualizer(prefab, movable);
+			}
+		}
+
+		private static void AddVisualizer(GameObject go, bool movable)
+		{
+			StationaryChoreRangeVisualizer choreRangeVisualizer = go.AddOrGet<StationaryChoreRangeVisualizer>();
+			choreRangeVisualizer.width = AdvancedRoboMiner.Range.width;
+			choreRangeVisualizer.height = AdvancedRoboMiner.Range.height;
+			choreRangeVisualizer.x = AdvancedRoboMiner.Range.xOffset;
+			choreRangeVisualizer.y = 0;
+			choreRangeVisualizer.vision_offset = AdvancedRoboMiner.Range.VisionOffset;
+			choreRangeVisualizer.movable = movable;
+		}
+	}
+}
---
+++ src/HeavyRoboMiner/AutoMinerPatches.cs
@@ -0,0 +1,33 @@
+﻿/// <summary>
+/// Created by lfricken https://github.com/lfricken/oni-mods
+/// </summary>
+
+using Harmony;
+
+namespace AdvancedRoboMiner
+{
+	class AutoMinerPatches
+	{
+		[HarmonyPatch(typeof(AutoMiner))]
+		[HarmonyPatch(nameof(ValidDigCell))]
+		public static class ValidDigCell
+		{
+			static bool Prefix() { return true; } // skip original method
+			static void Postfix(int cell, ref bool __result)
+			{
+				__result = Grid.Solid[cell] && !Grid.Foundation[cell] && Grid.Element[cell].hardness < AdvancedRoboMiner.MaxHardness;
+			}
+		}
+
+		[HarmonyPatch(typeof(AutoMiner))]
+		[HarmonyPatch(nameof(AutoMiner.DigBlockingCB))]
+		public static class DigBlockingCB
+		{
+			static bool Prefix() { return true; } // skip original method
+			static void Postfix(int cell, ref bool __result)
+			{
+				__result = Grid.Foundation[cell] || Grid.Element[cell].hardness >= AdvancedRoboMiner.MaxHardness;
+			}
+		}
+	}
+}
